392f20c6f70c5b89608128c54bfa5811a7ab2d17
Mjacob <maxime-jacob@etud.univ-tln.fr>
2023-10-25 16:44:29 +0200

    Etape 1 allumer led orange

--- zephyr_smart_home_OLANDA_JACOB/src/main.c
+++ zephyr_smart_home_OLANDA_JACOB/src/main.c
@@ -1,3 +1,32 @@
 //
 // Created by seatech on 25/10/23.
 //
+
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+#define LED_YELLOW_NODE DT_ALIAS(led_yellow)
+
+const struct gpio_dt_spec led_yellow_gpio = GPIO_DT_SPEC_GET_OR(LED_YELLOW_NODE, gpios, {0});
+
+int main(void){
+    gpio_pin_configure_dt(&led_yellow_gpio, GPIO_OUTPUT_HIGH);
+}
+/*
+void thread_function(){
+    // Do something
+}
+
+K_THREAD_DEFINE(thread_id, 521, thread_function, NULL, NULL, NULL, 9, 0, 0);
+
+extern void my_entry_point(void *, void *, void *);
+
+K_THREAD_STACK_DEFINE(my_stack_area, MY_STACK_SIZE);
+struct k_thread my_thread_data;
+
+k_tid_t my_tid = k_thread_create(&my_thread_data, my_stack_area,
+                                 K_THREAD_STACK_SIZEOF(my_stack_area),
+                                 my_entry_point,
+                                 NULL, NULL, NULL,
+                                 MY_PRIORITY, 0, K_NO_WAIT);
+*/
