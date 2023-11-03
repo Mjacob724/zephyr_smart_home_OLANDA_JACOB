a32293f1c04e57cd3c5fd3736d3ea0b985a1171d
Mjacob <maxime-jacob@etud.univ-tln.fr>
2023-11-03 17:01:14 +0100

    boutton

--- zephyr_smart_home_OLANDA_JACOB/src/main.c
+++ zephyr_smart_home_OLANDA_JACOB/src/main.c
@@ -32,6 +32,7 @@ const struct gpio_dt_spec button_gpio1 = GPIO_DT_SPEC_GET_OR(BUTTON_NODE_1, gpio
 const struct gpio_dt_spec button_gpio2 = GPIO_DT_SPEC_GET_OR(BUTTON_NODE_2, gpios, {0});
 const struct gpio_dt_spec buzzer_gpio = GPIO_DT_SPEC_GET_OR(BUZZER_NODE, gpios, {0});
 const struct gpio_dt_spec MotionSensor = GPIO_DT_SPEC_GET_OR(MOTION_SENSOR, gpios, {0});
+const struct device *const dht11 = DEVICE_DT_GET_ONE(aosong_dht);
 
 int flag = 0;
 
@@ -92,7 +93,39 @@ int main(void) {
     wifi_status();
     k_sem_take(&ipv4_address_obtained, K_FOREVER);
 
+
+    struct sensor_value temp, humidity, press;
     while(1){
+        struct sensor_value temp, humidity, press;
+
+        if (sensor_sample_fetch(dht11) < 0)
+        {
+            printk("Échec de l'échantillonnage du capteur DTH11\n");
+        }
+
+        if (sensor_channel_get(dht11, SENSOR_CHAN_AMBIENT_TEMP, &temp) < 0)
+        {
+            printk("Échec de récupération de la température\n");
+        }
+
+        if (sensor_channel_get(dht11, SENSOR_CHAN_HUMIDITY, &humidity) < 0)
+        {
+            printk("Échec de récupération de l'humidité\n");
+        }
+
+        if (sensor_channel_get(dht11, SENSOR_CHAN_PRESS, &press) < 0)
+        {
+            printk("Échec de récupération de la pression\n");
+        }
+
+        sensor_sample_fetch(dht11);
+        sensor_channel_get(dht11,SENSOR_CHAN_AMBIENT_TEMP,&temp);
+        sensor_channel_get(dht11,SENSOR_CHAN_HUMIDITY,&humidity);
+        sensor_channel_get(dht11,SENSOR_CHAN_PRESS,&press);
+
+        printk("temp: %d.%06d; press: %d.%06d; humidity: %d.%06d\n",
+               temp.val1, temp.val2, press.val1, press.val2,
+               humidity.val1, humidity.val2);
         k_sleep(K_SECONDS(10));
     }
 }
@@ -124,6 +157,17 @@ void alarm_thread(){
             write_lcd(&dev_lcd_screen, "ALARM MODE :", LCD_LINE_1);
             write_lcd(&dev_lcd_screen, "OFF          ", LCD_LINE_2);
             gpio_pin_configure_dt(&led_yellow_gpio, GPIO_OUTPUT_LOW);
+
+            struct sensor_value temp, humidity, press;
+
+            sensor_sample_fetch(dht11);
+            sensor_channel_get(dht11,SENSOR_CHAN_AMBIENT_TEMP,&temp);
+            sensor_channel_get(dht11,SENSOR_CHAN_HUMIDITY,&humidity);
+            sensor_channel_get(dht11,SENSOR_CHAN_PRESS,&press);
+
+            printk("temp: %d.%06d; press: %d.%06d; humidity: %d.%06d\n",
+                   temp.val1, temp.val2, press.val1, press.val2,
+                   humidity.val1, humidity.val2);
         }
         k_sleep(K_SECONDS(1));
     }
@@ -138,8 +182,8 @@ void gpio_callback_1()
 
 void gpio_callback_2()
 {
-    flag = 0;
     printk("Bouton 2 appuyé\n");
+    flag = 0;
 }
 
 void error()
