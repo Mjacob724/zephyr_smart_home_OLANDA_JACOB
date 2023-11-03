b677c69876e0100a273c587e7ef256069643c7e9
Mjacob <maxime-jacob@etud.univ-tln.fr>
2023-11-03 10:39:10 +0100

    boutton

--- zephyr_smart_home_OLANDA_JACOB/src/buzzer.c
+++ zephyr_smart_home_OLANDA_JACOB/src/buzzer.c
@@ -5,57 +5,11 @@
 
 const struct gpio_dt_spec buzzer_gpio = GPIO_DT_SPEC_GET_OR(BUZZER_NODE, gpios, {0});
 
-#define MIN_PERIOD PWM_SEC(1U) / 128U
-#define MAX_PERIOD PWM_SEC(1U)
-
 int main(void)
-{
-    uint32_t max_period;
-    uint32_t period;
-    uint8_t dir = 0U;
-    int ret;
-
-    printk("PWM-based blinky\n");
-
-    if (!pwm_is_ready_dt(&buzzer_gpio)) {
-        printk("Error: PWM device %s is not ready\n",
-               buzzer_gpio.dev->name);
-        return 0;
-    }
-
-    printk("Calibrating for channel %d...\n", buzzer_gpio.channel);
-    max_period = MAX_PERIOD;
-
-    while (pwm_set_dt(&buzzer_gpio, max_period, max_period / 2U))
-    {
-        max_period /= 2U;
-        if (max_period < (4U * MIN_PERIOD)) {
-            printk("Error: PWM device ",4U * MIN_PERIOD);
-            return 0;
-        }
-    }
-
-    printk("Done calibrating; maximum/minimum periods %u/%lu nsec\n",
-           max_period, MIN_PERIOD);
-
-    period = max_period;
-    while (1) {
-        ret = pwm_set_dt(&buzzer_gpio, period, period / 2U);
-        if (ret) {
-            printk("Error %d: failed to set pulse width\n", ret);
-            return 0;
-        }
-
-        period = dir ? (period * 2U) : (period / 2U);
-        if (period > max_period) {
-            period = max_period / 2U;
-            dir = 0U;
-        } else if (period < MIN_PERIOD) {
-            period = MIN_PERIOD * 2U;
-            dir = 1U;
-        }
-
-        k_sleep(K_SECONDS(4U));
-    }
-    return 0;
+        {
+    k_sleep(K_MSEC(1));
+    gpio_pin_configure_dt(&buzzer_gpio, GPIO_OUTPUT_LOW);
+    k_sleep(K_MSEC(1));
+    gpio_pin_configure_dt(&buzzer_gpio, GPIO_OUTPUT_HIGH);
+    k_sleep(K_MSEC(1));
 }
--- zephyr_smart_home_OLANDA_JACOB/src/main.c
+++ zephyr_smart_home_OLANDA_JACOB/src/main.c
@@ -32,7 +32,7 @@ const struct gpio_dt_spec capteur_pres_gpio = GPIO_DT_SPEC_GET_OR(CAPTEURPRES_NO
 void error();
 void gpio_callback_1();
 void gpio_callback_2();
-void buzzer_thread()
+
 
 int main(void)
 {
@@ -44,8 +44,7 @@ int main(void)
     write_lcd(&dev_lcd_screen, HELLO_MSG, LCD_LINE_1);
     write_lcd(&dev_lcd_screen, ZEPHYR_MSG, LCD_LINE_2);
 
-    if (!dht11)
-    {
+    if (!dht11) {
         error();
     }
 
@@ -53,48 +52,49 @@ int main(void)
     struct gpio_callback button_2;
 
     // Configurez les broches
-    gpio_pin_configure(button_gpio1.port, button_gpio1.pin, GPIO_INPUT );
+    gpio_pin_configure(button_gpio1.port, button_gpio1.pin, GPIO_INPUT);
     // Configurez la gestion des interruptions
-    gpio_init_callback(&button_1, gpio_callback_1, BIT(button_gpio1.pin) );
+    gpio_init_callback(&button_1, gpio_callback_1, BIT(button_gpio1.pin));
     gpio_add_callback(button_gpio1.port, &button_1);
     // Activez les interruptions
     gpio_pin_interrupt_configure_dt(&button_gpio1, GPIO_INT_EDGE_BOTH);
 
 
-    gpio_pin_configure(button_gpio2.port, button_gpio2.pin, GPIO_INPUT );
-    gpio_init_callback(&button_2, gpio_callback_2, BIT(button_gpio2.pin) );
+    gpio_pin_configure(button_gpio2.port, button_gpio2.pin, GPIO_INPUT);
+    gpio_init_callback(&button_2, gpio_callback_2, BIT(button_gpio2.pin));
     gpio_add_callback(button_gpio2.port, &button_2);
     gpio_pin_interrupt_configure_dt(&button_gpio2, GPIO_INT_EDGE_BOTH);
 
+    k_sleep(K_MSEC(1));
+    gpio_pin_configure_dt(&buzzer_gpio, GPIO_OUTPUT_LOW);
+    k_sleep(K_MSEC(1));
+    gpio_pin_configure_dt(&buzzer_gpio, GPIO_OUTPUT_HIGH);
+    k_sleep(K_MSEC(1));
+    printk("Le buzzer est activé\n");
 
-    while (1)
-    {
+    while (1) {
         struct sensor_value temp, humidity, press;
 
-        if (sensor_sample_fetch(dht11) < 0)
-        {
+        if (sensor_sample_fetch(dht11) < 0) {
             printk("Échec de l'échantillonnage du capteur DTH11\n");
         }
 
-        if (sensor_channel_get(dht11, SENSOR_CHAN_AMBIENT_TEMP, &temp) < 0)
-        {
+        if (sensor_channel_get(dht11, SENSOR_CHAN_AMBIENT_TEMP, &temp) < 0) {
             printk("Échec de récupération de la température\n");
         }
 
-        if (sensor_channel_get(dht11, SENSOR_CHAN_HUMIDITY, &humidity) < 0)
-        {
+        if (sensor_channel_get(dht11, SENSOR_CHAN_HUMIDITY, &humidity) < 0) {
             printk("Échec de récupération de l'humidité\n");
         }
 
-        if (sensor_channel_get(dht11, SENSOR_CHAN_PRESS, &press) < 0)
-        {
+        if (sensor_channel_get(dht11, SENSOR_CHAN_PRESS, &press) < 0) {
             printk("Échec de récupération de la pression\n");
         }
 
         sensor_sample_fetch(dht11);
-        sensor_channel_get(dht11,SENSOR_CHAN_AMBIENT_TEMP,&temp);
-        sensor_channel_get(dht11,SENSOR_CHAN_HUMIDITY,&humidity);
-        sensor_channel_get(dht11,SENSOR_CHAN_PRESS,&press);
+        sensor_channel_get(dht11, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+        sensor_channel_get(dht11, SENSOR_CHAN_HUMIDITY, &humidity);
+        sensor_channel_get(dht11, SENSOR_CHAN_PRESS, &press);
 
         printk("temp: %d.%06d; press: %d.%06d; humidity: %d.%06d\n",
                temp.val1, temp.val2, press.val1, press.val2,
@@ -104,38 +104,6 @@ int main(void)
     }
 
 
-    uint32_t max_period;
-    uint32_t period;
-    uint8_t dir = 0U;
-    int ret;
-
-    printk("PWM-based blinky\n");
-
-    if (!pwm_is_ready_dt(&buzzer_gpio)) {
-        printk("Error: PWM device %s is not ready\n",
-               buzzer_gpio.port->name);
-        return 0;
-    }
-
-    //printk("Calibrating for channel %d...\n", buzzer_gpio.channel);
-    max_period = MAX_PERIOD;
-
-    while (pwm_set_dt(&buzzer_gpio, max_period, max_period / 2U))
-    {
-        max_period /= 2U;
-        if (max_period < (4U * MIN_PERIOD))
-        {
-            printk("Error: PWM device ",4U * MIN_PERIOD);
-            return 0;
-        }
-    }
-
-    printk("Done calibrating; maximum/minimum periods %u/%lu nsec\n",
-           max_period, MIN_PERIOD);
-
-    period = max_period;
-
-    void buzzer_thread()
 }
 
 void error()
@@ -156,31 +124,4 @@ void gpio_callback_2()
     printk("Bouton 2 appuyé\n");
 }
 
-void buzzer_thread()
-{
-    while ()
-    {
-        ret = pwm_set_dt(&buzzer_gpio, period, period / 2U);
-        if (ret)
-        {
-            printk("Error %d: failed to set pulse width\n", ret);
-            return 0;
-        }
 
-        period = dir ? (period * 2U) : (period / 2U);
-        if (period > max_period)
-        {
-            period = max_period / 2U;
-            dir = 0U;
-        }
-        else if (period < MIN_PERIOD)
-        {
-            period = MIN_PERIOD * 2U;
-            dir = 1U;
-        }
-
-        k_sleep(K_SECONDS(4U));
-    }
-    return 0;
-}
-//K_THREAD_DEFINE(compute_thread_id,521, compute_thread, NULL, NULL, NULL, 9, 0, 0);
