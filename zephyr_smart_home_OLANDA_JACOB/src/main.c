556096f0c2df177aa2880afbdcfb91bda3b54781
Mjacob <maxime-jacob@etud.univ-tln.fr>
2023-11-03 16:13:48 +0100

    boutton

--- zephyr_smart_home_OLANDA_JACOB/src/main.c
+++ zephyr_smart_home_OLANDA_JACOB/src/main.c
@@ -1,7 +1,7 @@
 //
 // Created by seatech on 03/11/23.
 //
-
+#include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
 #include "../inc/lcd_screen_i2c.h"
@@ -9,6 +9,13 @@
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/drivers/pwm.h>
+#include <zephyr/drivers/adc.h>
+#include <zephyr/net/net_if.h>
+#include <zephyr/net/wifi_mgmt.h>
+#include <zephyr/net/net_event.h>
+#include <zephyr/net/http/client.h>
+#include <errno.h>
+#include <zephyr/net/socket.h>
 
 #define LED_YELLOW_NODE DT_ALIAS(led_yellow)
 #define LCD_NODE DT_ALIAS(afficheur_lcd)
@@ -16,39 +23,8 @@
 #define BUTTON_NODE_2 DT_ALIAS(button2)
 #define BUZZER_NODE DT_ALIAS(buzzer)
 #define MOTION_SENSOR DT_ALIAS(motion_sensor)
-
-CONFIG_WIFI=y
-CONFIG_INIT_STACKS=y
-CONFIG_NET_L2_WIFI_MGMT=y
-CONFIG_HEAP_MEM_POOL_SIZE=98304
-CONFIG_NET_L2_ETHERNET=y
-CONFIG_ESP32_WIFI_STA_AUTO_DHCPV4=y
-
-CONFIG_NETWORKING=y
-CONFIG_NET_IPV4=y
-CONFIG_NET_IPV6=n
-CONFIG_NET_UDP=y
-CONFIG_NET_TCP=y
-CONFIG_DNS_RESOLVER=y
-CONFIG_DNS_RESOLVER_AI_MAX_ENTRIES=10
-CONFIG_NET_SOCKETS_POSIX_NAMES=y
-CONFIG_NET_SOCKETS=y
-CONFIG_HTTP_CLIENT=y
-
-CONFIG_NET_DHCPV4=y
-CONFIG_NET_CONFIG_SETTINGS=y
-CONFIG_NET_TX_STACK_SIZE=2048
-CONFIG_NET_RX_STACK_SIZE=2048
-CONFIG_NET_PKT_TX_COUNT=10
-CONFIG_NET_PKT_RX_COUNT=10
-CONFIG_NET_BUF_RX_COUNT=20
-CONFIG_NET_BUF_TX_COUNT=20
-CONFIG_NET_MAX_CONTEXTS=10
-
-
-
-
-
+#define SSID "OnePlus 8T"
+#define PSK "eagleoffire8"
 
 const struct gpio_dt_spec led_yellow_gpio = GPIO_DT_SPEC_GET_OR(LED_YELLOW_NODE, gpios, {0});
 const struct i2c_dt_spec dev_lcd_screen = I2C_DT_SPEC_GET(LCD_NODE);
@@ -59,10 +35,23 @@ const struct gpio_dt_spec MotionSensor = GPIO_DT_SPEC_GET_OR(MOTION_SENSOR, gpio
 
 int flag = 0;
 
+static K_SEM_DEFINE(wifi_connected, 0, 1);
+static K_SEM_DEFINE(ipv4_address_obtained, 0, 1);
+
+static struct net_mgmt_event_callback wifi_cb;
+static struct net_mgmt_event_callback ipv4_cb;
+
 void alarm_thread();
 void error();
 void gpio_callback_1();
 void gpio_callback_2();
+
+static void handle_wifi_connect_result(struct net_mgmt_event_callback *cb);
+static void handle_wifi_disconnect_result(struct net_mgmt_event_callback *cb);
+static void handle_ipv4_result(struct net_if *iface);
+static void wifi_mgmt_event_handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_event, struct net_if *iface);
+void wifi_connect(void);
+void wifi_status(void);
 
 #define MIN_PERIOD PWM_SEC(1U) / 128U
 #define MAX_PERIOD PWM_SEC(1U)
@@ -90,6 +79,18 @@ int main(void) {
     write_lcd(&dev_lcd_screen, "OFF           ", LCD_LINE_2);
 
     gpio_pin_configure_dt(&MotionSensor, GPIO_INPUT);
+
+    net_mgmt_init_event_callback(&wifi_cb, wifi_mgmt_event_handler, NET_EVENT_WIFI_CONNECT_RESULT | NET_EVENT_WIFI_DISCONNECT_RESULT);
+    net_mgmt_init_event_callback(&ipv4_cb, wifi_mgmt_event_handler, NET_EVENT_IPV4_ADDR_ADD);
+
+    net_mgmt_add_event_callback(&wifi_cb);
+    net_mgmt_add_event_callback(&ipv4_cb);
+
+    wifi_connect();
+
+    k_sem_take(&wifi_connected, K_FOREVER);
+    wifi_status();
+    k_sem_take(&ipv4_address_obtained, K_FOREVER);
 
     while(1){
         k_sleep(K_SECONDS(10));
@@ -145,3 +146,120 @@ void error()
 {
 }
 
+static void handle_wifi_connect_result(struct net_mgmt_event_callback *cb)
+{
+    const struct wifi_status *status = (const struct wifi_status *)cb->info;
+    if (status->status)
+    {
+        printk("Connection request failed (%d)\n", status->status);
+    }
+    else
+    {
+        printk("Connected\n");
+        k_sem_give(&wifi_connected);
+    }
+}
+
+static void handle_wifi_disconnect_result(struct net_mgmt_event_callback *cb)
+{
+    const struct wifi_status *status = (const struct wifi_status *)cb->info;
+    if (status->status)
+    {
+        printk("Disconnection request (%d)\n", status->status);
+    }
+    else
+    {
+        printk("Disconnected\n");
+        k_sem_take(&wifi_connected, K_NO_WAIT);
+    }
+}
+
+static void handle_ipv4_result(struct net_if *iface)
+{
+    int i = 0;
+    for (i = 0; i < NET_IF_MAX_IPV4_ADDR; i++)
+    {
+        char buf[NET_IPV4_ADDR_LEN];
+        if (iface->config.ip.ipv4->unicast[i].addr_type != NET_ADDR_DHCP)
+        {
+            continue;
+        }
+        printk("IPv4 address: %s\n", net_addr_ntop(AF_INET, &iface->config.ip.ipv4->unicast[i].address.in_addr, buf, sizeof(buf)));
+        printk("Subnet: %s\n", net_addr_ntop(AF_INET, &iface->config.ip.ipv4->netmask, buf, sizeof(buf)));
+        printk("Router: %s\n", net_addr_ntop(AF_INET, &iface->config.ip.ipv4->gw, buf, sizeof(buf)));
+    }
+    k_sem_give(&ipv4_address_obtained);
+}
+
+static void wifi_mgmt_event_handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_event, struct net_if *iface)
+{
+    switch (mgmt_event)
+    {
+        case NET_EVENT_WIFI_CONNECT_RESULT:
+            handle_wifi_connect_result(cb);
+            break;
+        case NET_EVENT_WIFI_DISCONNECT_RESULT:
+            handle_wifi_disconnect_result(cb);
+            break;
+        case NET_EVENT_IPV4_ADDR_ADD:
+            handle_ipv4_result(iface);
+            break;
+        default:
+            break;
+    }
+}
+
+void wifi_connect(void)
+{
+    struct net_if *iface = net_if_get_default();
+    struct wifi_connect_req_params wifi_params = {0};
+    wifi_params.ssid = SSID;
+    wifi_params.psk = PSK;
+    wifi_params.ssid_length = strlen(SSID);
+    wifi_params.psk_length = strlen(PSK);
+    wifi_params.channel = WIFI_CHANNEL_ANY;
+    wifi_params.security = WIFI_SECURITY_TYPE_PSK;
+    wifi_params.band = WIFI_FREQ_BAND_2_4_GHZ;
+    wifi_params.mfp = WIFI_MFP_OPTIONAL;
+    printk("Connecting to SSID: %s\n", wifi_params.ssid);
+    if (net_mgmt(NET_REQUEST_WIFI_CONNECT, iface, &wifi_params, sizeof(struct wifi_connect_req_params)))
+    {
+        printk("WiFi Connection Request Failed\n");
+    }
+}
+
+void wifi_status(void)
+{
+    struct net_if *iface = net_if_get_default();
+    struct wifi_iface_status status = {0};
+    if (net_mgmt(NET_REQUEST_WIFI_IFACE_STATUS, iface, &status, sizeof(struct wifi_iface_status)))
+    {
+        printk("WiFi Status Request Failed\n");
+    }
+    printk("\n");
+    if (status.state >= WIFI_STATE_ASSOCIATED)
+    {
+        printk("SSID: %-32s\n", status.ssid);
+        printk("Channel: %d\n", status.channel);
+        printk("RSSI: %d\n", status.rssi);
+    }
+}
+
+/*
+static struct addrinfo hints;
+struct addrinfo *res;
+int st, sock, ret;
+hints.ai_family = AF_INET;
+hints.ai_socktype = SOCK_STREAM;
+st = getaddrinfo(HTTP_HOST, HTTP_PORT, &hints, &res);
+printf("getaddrinfo status: %d\n", st);
+
+if (st != 0)
+{
+printf("Unable to resolve address, quitting\n");
+return 0;
+}
+dump_addrinfo(res);
+sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+CHECK(zsock_connect(sock, res->ai_addr, res->ai_addrlen));
+*/
